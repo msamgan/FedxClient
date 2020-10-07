7c95bcf97eba80b49c33c6410d235e2d52f2e05c
msamgan <mohdsamgankhan@gmail.com>
2020-10-07 11:33:55 +0400

    introducted helpers

--- src/Adapter/Adapter.php
+++ src/Adapter/Adapter.php
@@ -9,7 +9,6 @@ class Adapter
      */
     public function __construct()
     {
-        require_once(__DIR__ . '/Commons/fedex-common.php5');
         ini_set("soap.wsdl_cache_enabled", "0");
     }
 
